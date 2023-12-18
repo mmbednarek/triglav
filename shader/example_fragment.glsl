8448fe432de2faa854a8f522dd43ae9d1721b4cb
Bednarek Mikołaj <mikolaj@bednarek.xyz>
2023-12-18 21:40:47 +0100

    added specular

--- shader/example_fragment.glsl
+++ shader/example_fragment.glsl
@@ -12,16 +12,22 @@ const vec3 lightPosition = vec3(0, 0, -20);
 const vec3 black = vec3(0, 0, 0);
 const vec3 white = vec3(1, 1, 1);
 const float ambientStrength = 0.1;
+const float shinniness = 60.0;
 
 void main() {
     vec4 texPixel = texture(texSampler, fragTexCoord);
 
+    vec3 normalVec = normalize(fragNormal);
     vec3 ambient = ambientStrength * white;
     vec3 lightDir = normalize(lightPosition - fragPosition);
-//    vec3 lightDir = vec3(1.0, 0.0, 0.0);
-    float diff = max(dot(normalize(fragNormal), normalize(lightDir)), 0.0);
-    vec3 diffuse = diff * white;
+    float diffuse = max(dot(normalVec, normalize(lightDir)), 0.0);
+    float specular = 0.0;
+    if (diffuse > 0.0) {
+        vec3 viewVec = normalize(-fragPosition);
+        vec3 halfVec = normalize(viewVec + lightDir);
+        specular = pow(dot(halfVec, normalVec), shinniness);
+    }
 
-    vec3 result = (ambient + diffuse) * texPixel.rgb;
+    vec3 result = (ambient + diffuse + specular) * texPixel.rgb;
     outColor = vec4(result, 1);
 }
